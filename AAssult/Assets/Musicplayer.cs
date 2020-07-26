81a4bf226a7bf4289c3311e4b6ac4e3b477f6ddf
Blank <Blank@DESKTOP-759FKM4>
2020-07-26 20:43:31 +0500

    Movemnetdone

---
+++ AAssult/Assets/Musicplayer.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class Musicplayer : MonoBehaviour
+{
+    private void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        Invoke("loadgame", 2f);
+    }
+
+   void loadgame()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
---
+++ AAssult/Assets/Player.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class Player : MonoBehaviour
+{
+    [Tooltip("In ms^-1")][SerializeField] float speedx = 4f;
+    [Tooltip("In m")] [SerializeField] float xrange = 5f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float xthrow = CrossPlatformInputManager.GetAxis("Horizontal");
+        float offsetx = xthrow * speedx * Time.deltaTime;
+        float rawoffsetx = transform.localPosition.x + offsetx;
+        float clamppos = Mathf.Clamp(rawoffsetx, -xrange,xrange);
+        transform.localPosition = new Vector3(clamppos, transform.localPosition.y, transform.localPosition.z);
+    }
+}
